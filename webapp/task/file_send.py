6416617e550e15d71e369fa6212bccfbbe27c85c
yangxu <690943136@qq.com>
2018-05-02 18:31:23 +0800

    finish the file_send html

--- webapp/task/file_send.py
+++ webapp/task/file_send.py
@@ -5,7 +5,20 @@ Created on 2018年4月30日
 
 @author: yangxu
 '''
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,HttpResponse
+import os,sys
+import json
+root_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 def file_send(request):
+    if request.method == 'POST':
+        print request.FILES['file-fr[]']
+        handle_uploaded_file(request.FILES['file-fr[]'])
+        return HttpResponse(json.dumps({'status':'success'}))
     return render_to_response('task/file.html', {})
+
+def handle_uploaded_file(f):
+    destination = open(root_dir+'/statics/upload/'+str(f), 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
